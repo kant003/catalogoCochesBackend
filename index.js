0b0e018d70470e6de13376be53c4af37807b81b3
Angel Gonzalez <gonzalezm.angel@gmail.com>
2019-02-19 12:09:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const app = express();
 app.use(bodyParser.urlencoded( {extended:false} ));
 app.use(bodyParser.json());
 
+
+// midle ware
+app.use( (req, res, next) => {
+	//permitimos que las peticiones se puedan hacer desde cualquier sitio
+	res.header('Access-Control-Allow-Origin', '*')
+	//res.header('Access-Control-Allow-Origin', '192.168.0.11')
+	// configuramos las cabeceras que pueden llegar
+	res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method' )
+	// configuramos los métodos que nos pueden llegar
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+	next(); // para que se salga de esta función
+})
+
+
+
 app.get('/coche/:id', CocheController.getCoche);
 app.get('/coches', CocheController.getCoches);
 app.post('/coche', CocheController.saveCoche);
